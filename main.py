3b37ec2cfa72a7b3539b6700b10d2983e781ec51
Junaid Gehlot <junaid@pictory.ai>
2025-04-13 13:01:47 +0530

    Add .gitignore, main application logic, and initial personality scripts
    - Created .gitignore to exclude environment variable files.
    - Implemented main.py to load environment variables, manage chat interactions, and handle personality scripts.
    - Added initial personality scripts for 'hitesh' and 'piyush' to enhance chat functionality.

---
+++ main.py
@@ -0,0 +1,89 @@
+import json
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = OpenAI()
+client.api_key =  os.getenv("OPENAI_API_KEY") 
+
+personalities = [personality.replace('.txt', '') for personality in os.listdir('scripts') if personality.endswith('.txt')]
+messages=[]
+
+
+def generate_personality_script(personality, script):
+    return  f"""
+        You are an digital version of a {personality},
+
+        Following is the script contains the way he or she talks 
+        {{script: {script}}}
+
+        do not take any context from the sctipt
+
+        Rules:
+        1. follow thw strict JSON output as per output schema
+        
+        Output Format:
+        {{step: 'string', content: "string"}}   
+
+        Example:
+        Input: Hi
+        Output:{{step: "reply", content: {personality} answer}}
+
+        Example:
+        Input: bye
+        Output:{{step: "end", content: {personality} answer}}
+    """
+
+
+def retrieve_persona_script(personality):
+    index = personalities.index(personality)       
+    if index >= 0:
+        f = open(f'scripts/{personalities[index]}.txt', "r")
+        return f.read()
+    
+def add_chat_message(role, content):
+    messages.append({'role':role,'content':content})   
+
+    
+def construct_persona_response():  
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        response_format={"type": 'json_object'},
+        messages = messages
+    )
+    return response    
+
+
+def start(personality):  
+    print("Start your chat")
+    script= retrieve_persona_script(personality)
+    add_chat_message('system',generate_personality_script(personality, script))
+    while True:
+        user_chat = input(f'Start chatting with {personality}')       
+        add_chat_message('user',user_chat)       
+        response = construct_persona_response()
+        parsed_response = json.loads(response.choices[0].message.content)
+        print(parsed_response.get('content'))
+        if parsed_response.get('step') == 'end':
+            break
+
+
+
+start(personalities[1])
+        
+
+
+
+    
+
+
+
+       
+
+
+
+
+
+
+
